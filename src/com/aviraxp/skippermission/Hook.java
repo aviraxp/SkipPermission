cea42ea6de7af9547f074fef6569f04eba7c5e34
Wang Han <416810799@qq.com>
2016-09-22 23:47:49 -0500

    Bring up Project

---
+++ src/com/aviraxp/skippermission/Hook.java
@@ -0,0 +1,33 @@
+package com.aviraxp.skippermission;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Hook implements IXposedHookZygoteInit, IXposedHookLoadPackage {
+
+    public XC_MethodHook grantPermissionsBackButtonHook;
+
+    @Override
+    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
+
+        grantPermissionsBackButtonHook = new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param)
+                    throws Throwable {
+                //setResultAndFinish()
+                XposedHelpers.callMethod(param.thisObject, "setResultAndFinish");
+            }
+        };
+    }
+
+    @Override
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.android.packageinstaller") || lpparam.packageName.equals("com.google.android.packageinstaller") || lpparam.packageName.equals("com.mokee.packageinstaller")) {
+            XposedHelpers.findAndHookMethod("com.android.packageinstaller.permission.ui.GrantPermissionsActivity",
+                    lpparam.classLoader, "onCreate", grantPermissionsBackButtonHook);
+        }
+    }
+}
