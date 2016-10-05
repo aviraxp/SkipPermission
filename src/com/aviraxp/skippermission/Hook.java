e6f2efbac24fb7daf081574eb5c6a4a2cca215f8
Wang Han <416810799@qq.com>
2016-10-05 09:03:27 -0500

    Totally Rework and Fix every bug

--- src/com/aviraxp/skippermission/Hook.java
+++ src/com/aviraxp/skippermission/Hook.java
@@ -1,32 +1,23 @@
 package com.aviraxp.skippermission;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class Hook implements IXposedHookZygoteInit, IXposedHookLoadPackage {
-
-    public XC_MethodHook grantPermissionsBackButtonHook;
+public class Hook implements IXposedHookLoadPackage {
 
     @Override
-    public void initZygote(IXposedHookZygoteInit.StartupParam startupParam) throws Throwable {
-
-        grantPermissionsBackButtonHook = new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param)
-                    throws Throwable {
-                XposedHelpers.callMethod(param.thisObject, "setResultAndFinish");
-            }
-        };
-    }
-
-    @Override
-    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals("com.android.packageinstaller") || lpparam.packageName.equals("com.google.android.packageinstaller") || lpparam.packageName.equals("com.mokee.packageinstaller") || lpparam.packageName.equals("com.samsung.android.packageinstaller")) {
-            XposedHelpers.findAndHookMethod("com.android.packageinstaller.permission.ui.GrantPermissionsActivity",
-                    lpparam.classLoader, "onCreate", grantPermissionsBackButtonHook);
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if (lpparam.packageName.equals("com.android.packageinstaller")) {
+                XposedBridge.log("Load Packageinstaller");
+                XposedHelpers.findAndHookMethod("com.android.packageinstaller.permission.ui.GrantPermissionsActivity", lpparam.classLoader, "onCreate", android.os.Bundle.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedHelpers.callMethod(param.thisObject, "setResultAndFinish");
+                    }
+                });
         }
     }
 }
